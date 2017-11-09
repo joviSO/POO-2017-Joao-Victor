b2a01d081590b362789eb13b4455d5c664a6aa55
joviSO <levelupdojoao@hotmail.com>
2017-11-09 16:05:09 -0200

    Prova B

---
+++ POO-2017-Joao-Victor/Atividade B/Q2/src/Imagem1.java
@@ -0,0 +1,22 @@
+public class Imagem1 {
+	
+	public static void main(String [] args){
+		
+		int number = 1;
+		int sum = 0;
+		
+		if (number <= 100){
+			sum = sum + number;
+			number = number + 1;
+			
+		}
+		
+		else{
+		
+			System.out.println("acabou");
+		
+		}
+
+	}
+	
+}
---
+++ POO-2017-Joao-Victor/Atividade B/Q2/src/Imagem2.java
@@ -0,0 +1,14 @@
+public class Imagem2 {
+
+	public static void main(String [] args){
+		
+		int number = 1;
+		int sum = 0;
+	while (number < 1000000){
+		sum = sum += number;	
+		number = number +1;
+		System.out.println(number);
+		
+		}
+	}
+}
